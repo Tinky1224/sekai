b4efb57267271ac10b46d15d1ce5ec21c68f9716
Tinky <a37207607@gmail.com>
2022-06-08 13:10:57 +0800

    delete SECURE_SSL_REDIRECT

--- sekai/settings.py
+++ sekai/settings.py
@@ -30,7 +30,7 @@ SECURE_HSTS_SECONDS = 60
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-SECURE_SSL_REDIRECT = True
+#SECURE_SSL_REDIRECT = True
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
